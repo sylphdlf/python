190b0c1b3f3dcc1fdeb2296fb29b666185c930b3
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-06-09 15:01:28 +0800

    ～～～

--- FileScan/Utils/FileWatch.py
+++ FileScan/Utils/FileWatch.py
@@ -7,20 +7,20 @@ wm = pyinotify.WatchManager()
 
 class MyEventHandler(pyinotify.ProcessEvent):
 
-    def process_IN_MODIFY(event):
+    def process_IN_MODIFY(self, event):
         print('MODIFY-', event.pathname)
 
 
 notifier = pyinotify.Notifier(wm, MyEventHandler())
 
 
-def add_file_watch(path_):
-    wm.add_watch(path_, multi_event)
+# def add_file_watch(path_):
+wm.add_watch("/data/sh/log/registry.log", multi_event)
 
 
 notifier.loop()
 
-if __name__ == '__main__':
-    add_file_watch("/data/sh/log/registry.log")
+# if __name__ == '__main__':
+#     add_file_watch("/data/sh/log/registry.log")
 
 
