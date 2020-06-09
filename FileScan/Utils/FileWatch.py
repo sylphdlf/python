8ff7160ce85ecdf8b5b1287f57b4d824ce2184bc
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-06-09 14:58:33 +0800

    ～～～

--- FileScan/Utils/FileWatch.py
+++ FileScan/Utils/FileWatch.py
@@ -1,29 +1,26 @@
-# import pyinotify
-#
-#
-# multi_event = pyinotify.IN_MODIFY
-# wm = pyinotify.WatchManager()
-#
-#
-# class MyEventHandler(pyinotify.ProcessEvent):
-#
-#     def process_IN_MODIFY(event):
-#         print('MODIFY-', event.pathname)
-#         content = get_log_content(path)
-#         if len(str(content).strip()) != 0:
-#             send_msg(json.dumps({'key': str(path).rsplit(os.sep, 1)[1], 'value': content}))
-#
-#
-# notifier = pyinotify.Notifier(wm, MyEventHandler())
-#
-#
-# def add_file_watch(path_):
-#     wm.add_watch(path_, multi_event)
-#
-#
-# notifier.loop()
-#
-# if __name__ == '__main__':
-#     add_file_watch("/data/sh/log/registry.log")
-#
-#
+import pyinotify
+
+
+multi_event = pyinotify.IN_MODIFY
+wm = pyinotify.WatchManager()
+
+
+class MyEventHandler(pyinotify.ProcessEvent):
+
+    def process_IN_MODIFY(event):
+        print('MODIFY-', event.pathname)
+
+
+notifier = pyinotify.Notifier(wm, MyEventHandler())
+
+
+def add_file_watch(path_):
+    wm.add_watch(path_, multi_event)
+
+
+notifier.loop()
+
+if __name__ == '__main__':
+    add_file_watch("/data/sh/log/registry.log")
+
+
