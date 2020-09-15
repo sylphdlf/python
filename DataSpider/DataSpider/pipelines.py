ee21683a518df3d2b395f428f6676e63e1ad4292
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-09-15 15:32:37 +0800

    ～～～

--- DataSpider/DataSpider/pipelines.py
+++ DataSpider/DataSpider/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-from ..Utils import MysqlUtils
+from .MysqlUtils import MysqlUtils
 
 
 class DataspiderPipeline(object):
@@ -22,5 +22,5 @@ class AmapPipeline(object):
         sql = ["insert into p_amap(type, origin, destination, waypoints, time_cost, distance) "
                "value ({}, '{}', '{}', '{}', '{}', '{}')"
                    .format(item['type'], item['origin'], item['destination'], item['waypoints'], item['time_cost'], item['distance'])]
-        MysqlUtils.MysqlUtils.insert(sql)
+        MysqlUtils.insert(sql)
         return item
